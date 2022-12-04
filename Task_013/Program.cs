b6cc97484cd6f37b8a301b3b414696ed9658c4b8
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 11:41:04 +0800

    add Task 013

---
+++ Task_013/Program.cs
@@ -0,0 +1,22 @@
+﻿//напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли второе число кратным первому
+Console.Clear();
+Console.Write("Введите первое число: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else 
+{
+    int N = num1 / num2;
+
+Console.WriteLine("не кратно");
+Console.WriteLine($"остаток {num1 - num2*N}");
+}
