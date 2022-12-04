f92a109a1db12bb33b7137a2b35a0db00bbf1d57
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 20:01:49 +0800

    add Tsk 014 Home Work

---
+++ Task_014_HomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿// написать программу, которая выводит третью цифру заданного числа или сообщает, что ее нет
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 100)
+{
+   Console.WriteLine("третья цифра отсутствует"); 
+}
+else{
+while (N >= 1000)
+{
+    N = N / 10;
+}
+
+
+Console.WriteLine($"{N % 10}");
+}
+
