acf393ddb020cfc173e175c949fac1a5e0036efe
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 10:41:56 +0800

    add Task 012

---
+++ Task_012/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая выводит трехзначное число и удаляет вторую цифру этого числа
+Console.Clear();
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Число {num}");
+int num1 = num / 100 ;
+Console.Write($"{num1}");
+int num2 = num % 10 ;
+Console.WriteLine($"{num2}");
