e3c8e1170cc4b301292325baca188d1a8924ae75
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 10:59:45 +0800

    второй вариант с выводом

--- Task_012/Program.cs
+++ Task_012/Program.cs
@@ -6,3 +6,5 @@ int num1 = num / 100 ;
 Console.Write($"{num1}");
 int num2 = num % 10 ;
 Console.WriteLine($"{num2}");
+// второй вариант с выводом
+//Console.WriteLine($"{num} -> {num / 100}{num % 10}");
