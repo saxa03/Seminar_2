1d58361fb933da2dd64a097952397e38a01fcc51
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 20:23:52 +0800

    add Task 016 HomeWork

---
+++ Task_016_HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿// написать программу ,которая принимает на вход цифру, обозначающую день недели и
+//проверяет, является ли этот день выходным (6 или 7)
+Console.Clear();
+
+Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine());
+
+    if (num == 7 || num == 6)
+ {
+    Console.WriteLine("Выходной");
+ }
+    else
+ {
+    Console.WriteLine("Рабочий");
+ }
+ 
