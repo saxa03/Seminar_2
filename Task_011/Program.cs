58c5d745e22c41207b38cc9eec83b7799e0ba94f
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-04 10:27:04 +0800

    add Task 011

---
+++ Task_011/Program.cs
@@ -0,0 +1,15 @@
+﻿//напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру этого числа
+Console.Clear();
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Число {num}");
+int num1 = num / 10; //получаем первую цифру
+int num2 = num % 10; //получае последнюю цифру
+ if (num1 > num2)
+ {
+    Console.WriteLine($"Цифра {num1} больше цифры {num2}");
+ }
+ else 
+ {
+    Console.WriteLine($"Цифра {num2} больше цифры {num1}");
+ }
